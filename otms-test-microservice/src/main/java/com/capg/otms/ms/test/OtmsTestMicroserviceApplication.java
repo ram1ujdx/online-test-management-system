25f1ca464ba87f5297f441941a2ab9cd6df8d363
ramanujdas <ramanujds9@gmail.com>
2020-08-10 16:04:45 +0530

    Initial Commit

---
+++ otms-test-microservice/src/main/java/com/capg/otms/ms/test/OtmsTestMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.capg.otms.ms.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtmsTestMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtmsTestMicroserviceApplication.class, args);
+	}
+
+}
---
+++ otms-test-microservice/src/test/java/com/capg/otms/ms/test/OtmsTestMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.otms.ms.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtmsTestMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
