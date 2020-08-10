bdca0abbf081850364109f244ef525cc108c7e14
praveen bandi <praveen bandi@LAPTOP-J82473UA>
2020-08-10 16:12:10 +0530

    otms question

---
+++ otms-question-microservice/src/main/java/com/capg/otms/ms/question/OtmsQuestionMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.capg.otms.ms.question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtmsQuestionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtmsQuestionMicroserviceApplication.class, args);
+	}
+
+}
---
+++ otms-question-microservice/src/test/java/com/capg/otms/ms/question/OtmsQuestionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.otms.ms.question;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtmsQuestionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
